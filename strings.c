8ade339fc4d9d7901cffc1a718430622bab148ac
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-11-23 23:18:47 +0100

    fix typo in strings
    argumends -> arguments

--- strings.c
+++ strings.c
@@ -3,6 +3,6 @@
 
 const char* PS1 = "Enter a command: ";
 
-const char* PS2 = "Enter an argumend, or nothing to end arguments: ";
+const char* PS2 = "Enter an argument, or nothing to end arguments: ";
 
 const char* EXIT = "exit";
