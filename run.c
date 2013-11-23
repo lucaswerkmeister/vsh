7d482a38dabd12d44613da2a6e5cbcaf46f35729
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-11-23 21:50:37 +0100

    Better file-not-found handling

--- run.c
+++ run.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 
@@ -8,11 +10,20 @@
 
 
 void run(char* command) {
+  // check if the file exists
+  if(access(command, X_OK) != 0) {
+    printf("Command not found or permission denied\n");
+    return;
+  }
+
+  // run
   pid_t process;
   if( (process = fork()) ) {
     waitpid(process, NULL, 0);
   } else {
     execl(command, command, (char*) NULL);
-    exit(-1); // in case the file was not found
+    // only executed if execution failed:
+    printf("Executing command \"%s\" failed due to error: %s\n", command, strerror(errno));
+    exit(-1);
   }
 }
