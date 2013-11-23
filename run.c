310418f34ccc777fda3253e0c381bd8ff6e62716
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-11-23 16:01:33 +0100

    run the supplied command
    There’s no way to supply arguments yet.

--- run.c
+++ run.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 #include "run.h"
 
 
 void run(char* command) {
-  printf("I want to run %s\n", command);
+  pid_t process;
+  if( (process = fork()) ) {
+    waitpid(process, NULL, 0);
+  } else {
+    execl(command, command, (char*) NULL);
+  }
 }
